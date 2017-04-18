764ea5f0e36ad624f52e44d28b5b2336b2851f08
Janghoon Lee <chaossky@naver.com>
2017-04-18 23:15:12 +0900

    조금 새로이 수정했어요

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -1,11 +1,22 @@
-// ConsoleApplication5.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
-//
-
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	int numberOfLanguages;
+
+	cout << "Hello Reader\n" 
+		<< "Welcome to C++\n";
+
+	cout << "How many languages did you know?";
+
+	cin >> numberOfLanguages;
+
+	if (numberOfLanguages > 1)
+		cout << "Keep Going!!!!\n ";
+	else
+		cout << "Study Hard\n";
     return 0;
 }
 
